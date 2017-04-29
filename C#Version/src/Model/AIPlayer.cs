11bde8b21194804c7032600f6330d84845e0a01f
anonymousxk <hjhk1995dn@gmail.com>
2017-04-29 17:44:52 +1000

    fix-stopping-game-bugs

--- C#Version/src/Model/AIPlayer.cs
+++ C#Version/src/Model/AIPlayer.cs
@@ -61,20 +61,22 @@ public abstract class AIPlayer : Player
 		/// <returns>true if location 1 and location 2 are at the same spot</returns>
 		public static bool operator ==(Location @this, Location other)
 		{
-			return @this != null && other != null && @this.Row == other.Row && @this.Column == other.Column;
-		}
+            //return @this != null && other != null && @this.Row == other.Row && @this.Column == other.Column;
+            return !ReferenceEquals(@this, null) && !ReferenceEquals(other, null) && @this.Row == other.Row && @this.Column == other.Column;
+        }
 
-		/// <summary>
-		/// Check if two locations are not equal
-		/// </summary>
-		/// <param name=this>location 1</param>
-		/// <param name=other>location 2</param>
-		/// <returns>true if location 1 and location 2 are not at the same spot</returns>
-		public static bool operator !=(Location @this, Location other)
+        /// <summary>
+        /// Check if two locations are not equal
+        /// </summary>
+        /// <param name=this>location 1</param>
+        /// <param name=other>location 2</param>
+        /// <returns>true if location 1 and location 2 are not at the same spot</returns>
+        public static bool operator !=(Location @this, Location other)
 		{
-			return @this == null || other == null || @this.Row != other.Row || @this.Column != other.Column;
-		}
-	}
+			//return @this == null || other == null || @this.Row != other.Row || @this.Column != other.Column;
+            return ReferenceEquals(@this, null) || ReferenceEquals(other, null) || @this.Row != other.Row || @this.Column != other.Column;
+        }
+    }
 
 
 	public AIPlayer(BattleShipsGame game) : base(game)
